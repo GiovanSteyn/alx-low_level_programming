a9588402056199555842752f2a3d68caaddf407a
GiovanSteyn <giovansteyn@gmail.com>
2023-02-21 10:46:23 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,10 +2,8 @@
 
 /**
  * prints the last digit of a number
- * @n:  is the int that will use for the argument of the function
  * Return: integer value
  */
-
 int print_last_digit(int n)
 {
 	int last;
