c12192263956f1a310eeefedbb3e91c3771adad7
GiovanSteyn <giovansteyn@gmail.com>
2023-02-22 11:20:20 -0800

    work 4

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-        int largest;
+	int largest;
 
-        if (a > b)
-        {
-        if (b > c)
-                largest = a;
-        else if (a > c)
-                largest = a;
-        else
-                largest = c;
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-        }
-        else
-        {
-                if (b < c)
-                        largest = c;
-                else
-                        largest = b;
-        }
-        return (largest);
+	return (largest);
 }
